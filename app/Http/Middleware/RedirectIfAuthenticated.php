4ae7ec433e2295c953670a80b2f4e1c14c6b2c47
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-04-26 10:51:04 +0530

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,14 +17,24 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, ...$guards)
+    public function handle($request, Closure $next, $guard = null)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        if (Auth::guard($guard)->check()) {
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if($guard == "student"){
+
+                //student was authenticated with student guard.
+                return redirect()->route('student.dashboard.index');
+            } elseif($guard == "web"){
+
+                //staff was authenticated with web guard.
+                return redirect()->route('admin.dashboard.index');
+            } else {
+                
+                //default guard.
+                return redirect()->route('admin.dashboard.index');
             }
+
         }
 
         return $next($request);
