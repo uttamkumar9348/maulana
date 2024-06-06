80d99df4eaf79901dfab665f67e3b85e2a99068b
srdcpvtltd <80140844+srdcpvtltd@users.noreply.github.com>
2024-06-06 22:15:30 +0530

    Update Result.php

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -8,13 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
-protected $dateFormat = 'Y-m-d';
     protected $fillable=[
       'user_id',
       'quiz_id',
       'quiz_score',
       'achieved_score'
     ];
+use Carbon\Carbon;
+
+protected $dates = ['created_at','updated_at'];
+
+protected function serializeDate(DateTimeInterface $dates)
+{
+    return Carbon::createFromFormat('Y-m-d H:i:s', $dates)->diffForHumans();
+    OR
+    //return Carbon::createFromFormat('Y-m-d H:i:s', $dates)->format('Y-m-d');
 
     public function quiz(){
         $this->belongsTo(Quiz::class);
