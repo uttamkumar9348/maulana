bb61b5791a85d212474fd1b383844e88cb1cfe4a
SatyamRoutray <satyamroutray143@gmail.com>
2024-06-06 18:03:43 +0530

    update

---
+++ app/Models/MockResult.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MockResults extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'mock_results';
+
+}
